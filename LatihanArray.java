06b077bf4d413eb1991630cb0148fe13709ece57
Roisah <roisah.02@gmail.com>
2016-04-04 14:58:23 +0700

    Added files via upload

---
+++ LatihanArray.java
@@ -0,0 +1,12 @@
+
+package latihanarray;
+public class LatihanArray {
+
+    public static void main(String[] args) {
+        int[] data = {20, 10, 50, 30, 10};
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Data ke " + i + " adalah : " + data[i]);
+    }
+    
+}
